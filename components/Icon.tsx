3a4b8366e9ded80c4f87446055debfd98b0528ee
ArunGovil <a.arungovil@gmail.com>
2022-12-08 20:32:12 +0530

    fix/icon-typescript

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -15,7 +15,7 @@ export default function Icon({
     .replace(/([a-z0-9])([A-Z])/g, "$1 $2")
     .split(" ")[0]
     .toLocaleLowerCase();
-  const ElementIcon: IconType = loadable(
+  const ElementIcon: any = loadable(
     () => import(`react-icons/${lib}/index.js`),
     {
       resolveComponent: (el: JSX.Element) => el[nameIcon as keyof JSX.Element],
