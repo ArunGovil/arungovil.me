e2c4737b7c1356323d8a4f174724a5eb882dc149
ArunGovil <a.arungovil@gmail.com>
2023-06-04 15:42:57 +0530

    update/font-family

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -16,7 +16,7 @@ export default function ProjectCard({
   const image = require(`../public/icons/${icon}.svg`);
   return (
     <Link href={`/posts/${url}`}>
-      <div className="rounded-md h-24 flex p-2 items-center hover:bg-gray-900 hover:bg-opacity-40">
+      <div className="rounded-md h-24 flex p-2 items-center hover:bg-slate-800 hover:bg-opacity-40">
         <Image alt="icon" src={image} width="42" className="ml-2" />
         <div className="pl-4">
           <p className="text-md max-w-[60vw] sm:max-w-[70vw] truncate">
